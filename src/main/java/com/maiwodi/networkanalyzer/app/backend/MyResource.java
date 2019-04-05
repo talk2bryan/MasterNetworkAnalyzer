3a133f4051c34f6ef7b170365d378f4f80cc6d23
jxmai <jxmai@users.noreply.github.com>
2019-04-04 19:47:41 -0500

    Restful backend setup

---
+++ src/main/java/com/maiwodi/networkanalyzer/app/backend/MyResource.java
@@ -0,0 +1,34 @@
+package com.maiwodi.networkanalyzer.app.backend;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent to the
+	 * client as "text/plain" media type.
+	 *
+	 * @return String that will be returned as a text/plain response.
+	 */
+	@GET
+	@Path("/hello")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getIt() {
+		return "Got it!";
+	}
+
+	@GET
+	@Path("/get")
+	@Produces(MediaType.APPLICATION_JSON)
+	public DummyModel sampleJson() {
+		return new DummyModel("afd", "ss");
+	}
+
+}
