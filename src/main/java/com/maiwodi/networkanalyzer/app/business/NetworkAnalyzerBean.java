de25f1d87c93080b4dc5565c63a0753870885e08
jxmai <jxmai@users.noreply.github.com>
2019-04-11 00:51:56 -0500

    refactoring

---
+++ src/main/java/com/maiwodi/networkanalyzer/app/business/NetworkAnalyzerBean.java
@@ -0,0 +1,53 @@
+package com.maiwodi.networkanalyzer.app.business;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import com.maiwodi.networkanalyzer.app.models.Worker;
+import com.maiwodi.networkanalyzer.app.models.Workers;
+import com.maiwodi.networkanalyzer.app.utils.AbstractPageBean;
+import com.maiwodi.networkanalyzer.app.utils.Utilities;
+
+@Named
+@ViewScoped
+public class NetworkAnalyzerBean extends AbstractPageBean {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 8692181958971244558L;
+
+    private Workers workers;
+
+    private Worker worker;
+
+    @PostConstruct
+    public void loadPage() {
+	workers = new Workers();
+	worker = new Worker();
+    }
+
+    public void submitWorker() {
+	workers.addWorker(worker);
+
+	Utilities.showInfoMessage("Worker Submitted", "The submitted worker IP is " + worker.getWorkerIP());
+    }
+
+    public Workers getWorkers() {
+	return workers;
+    }
+
+    public void setWorkers(Workers workers) {
+	this.workers = workers;
+    }
+
+    public Worker getWorker() {
+	return worker;
+    }
+
+    public void setWorker(Worker worker) {
+	this.worker = worker;
+    }
+
+}
