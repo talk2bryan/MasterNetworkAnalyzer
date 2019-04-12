a3a4b5ea1b276fdd93158cdbacaf5f7d75024d85
jxmai <jxmai@users.noreply.github.com>
2019-04-12 15:22:05 -0500

    clean up

--- src/main/java/com/maiwodi/networkanalyzer/app/backend/MasterWebServices.java
+++ src/main/java/com/maiwodi/networkanalyzer/app/backend/MasterWebServices.java
@@ -1,8 +1,5 @@
 package com.maiwodi.networkanalyzer.app.backend;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,9 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
 import javax.ws.rs.Consumes;
