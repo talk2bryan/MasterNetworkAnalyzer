5c35912390179d8baa99ae886a4785f3cb0deb61
jxmai <jxmai@users.noreply.github.com>
2019-04-12 14:16:29 -0500

    refactoring

--- test/com/maiwodi/networkanalyzer/tests/backend/TestMasterWebServices.java
+++ test/com/maiwodi/networkanalyzer/tests/backend/TestMasterWebServices.java
@@ -6,13 +6,17 @@ import javax.ws.rs.core.Response;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.maiwodi.networkanalyzer.app.utils.JerseyClient;
 
-class TestWorkerResource {
+class TestMasterWebServices {
 
-	private static final Logger LOGGER = LogManager.getLogger(TestWorkerResource.class.getName());
+	private static final Logger LOGGER = LogManager.getLogger(TestMasterWebServices.class.getName());
 
 	String jsonStr = "[ {\r\n" + "\r\n" + "        \"rssiValue\" : -55,\r\n" + "        \"speedInMbps\" : 72,\r\n"
 			+ "        \"timeStamp\" : \"1554603519\"\r\n" + "      }, {\r\n" + "        \"rssiValue\" : -55,\r\n"
@@ -36,18 +40,47 @@ class TestWorkerResource {
 			+ "        \"timeStamp\" : \"1554603543\"\r\n" + "      }, {\r\n" + "        \"rssiValue\" : -55,\r\n"
 			+ "        \"speedInMbps\" : 72,\r\n" + "        \"timeStamp\" : \"1554603545\"\r\n" + "      } ]";
 
-	@Test
-	void testNetworkDataAnalysis() {
-//		Response response = JerseyClient.sendPostResponse("http://localhost:8080/networkanalyzer/",
-//				"rest/worker/post/data", jsonStr);
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
 
-		Response response = JerseyClient.sendPostResponse("http://172.31.31.231:8080/networkanalyzer-1.0-SNAPSHOT/",
-				"rest/worker/post/data", jsonStr);
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	// @Test
+	// @Ignore
+	// void testAddWorker() {
+	// fail("Not yet implemented");
+	// }
+
+	@Test
+	void testPostDataForAnalysis() {
+		Response response = JerseyClient.sendPostResponse("http://localhost:8080/networkanalyzer/",
+				"rest/master/post/data", jsonStr);
 
 		LOGGER.debug("Response: {}", response.readEntity(String.class));
 
 		assertEquals(response.getStatus(), 200);
+	}
 
+	@Test
+	void testCloudPostDataForAnalysis() {
+		Response response = JerseyClient.sendPostResponse(
+				"http://ec2-3-94-173-58.compute-1.amazonaws.com:8080/networkanalyzer-1.0-SNAPSHOT/",
+				"rest/myresource/post/data", jsonStr);
+
+		LOGGER.debug("Response: {}", response.readEntity(String.class));
+
+		assertEquals(response.getStatus(), 200);
 	}
 
 }
--- test/com/maiwodi/networkanalyzer/tests/backend/TestWorkerWebServices.java
+++ test/com/maiwodi/networkanalyzer/tests/backend/TestWorkerWebServices.java
@@ -6,17 +6,13 @@ import javax.ws.rs.core.Response;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.maiwodi.networkanalyzer.app.utils.JerseyClient;
 
-class TestMyResource {
+class TestWorkerWebServices {
 
-	private static final Logger LOGGER = LogManager.getLogger(TestMyResource.class.getName());
+	private static final Logger LOGGER = LogManager.getLogger(TestWorkerWebServices.class.getName());
 
 	String jsonStr = "[ {\r\n" + "\r\n" + "        \"rssiValue\" : -55,\r\n" + "        \"speedInMbps\" : 72,\r\n"
 			+ "        \"timeStamp\" : \"1554603519\"\r\n" + "      }, {\r\n" + "        \"rssiValue\" : -55,\r\n"
@@ -40,47 +36,18 @@ class TestMyResource {
 			+ "        \"timeStamp\" : \"1554603543\"\r\n" + "      }, {\r\n" + "        \"rssiValue\" : -55,\r\n"
 			+ "        \"speedInMbps\" : 72,\r\n" + "        \"timeStamp\" : \"1554603545\"\r\n" + "      } ]";
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	// @Test
-	// @Ignore
-	// void testAddWorker() {
-	// fail("Not yet implemented");
-	// }
-
 	@Test
-	void testPostDataForAnalysis() {
-		Response response = JerseyClient.sendPostResponse("http://localhost:8080/networkanalyzer/",
-				"rest/master/post/data", jsonStr);
+	void testNetworkDataAnalysis() {
+//		Response response = JerseyClient.sendPostResponse("http://localhost:8080/networkanalyzer/",
+//				"rest/worker/post/data", jsonStr);
+
+		Response response = JerseyClient.sendPostResponse("http://172.31.31.231:8080/networkanalyzer-1.0-SNAPSHOT/",
+				"rest/worker/post/data", jsonStr);
 
 		LOGGER.debug("Response: {}", response.readEntity(String.class));
 
 		assertEquals(response.getStatus(), 200);
-	}
 
-	@Test
-	void testCloudPostDataForAnalysis() {
-		Response response = JerseyClient.sendPostResponse(
-				"http://ec2-3-94-173-58.compute-1.amazonaws.com:8080/networkanalyzer-1.0-SNAPSHOT/",
-				"rest/myresource/post/data", jsonStr);
-
-		LOGGER.debug("Response: {}", response.readEntity(String.class));
-
-		assertEquals(response.getStatus(), 200);
 	}
 
 }
